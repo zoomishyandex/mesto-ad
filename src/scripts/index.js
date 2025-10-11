1ea7893374c39c44df81c0124145295881ea454f
zoomishyandex <zoomish1@yandex.ru>
2025-10-11 18:53:07 +0300

    Initial commit

---
+++ src/scripts/index.js
@@ -0,0 +1,115 @@
+/*
+  Файл index.js является точкой входа в наше приложение
+  и только он должен содержать логику инициализации нашего приложения
+  используя при этом импорты из других файлов
+
+  Из index.js не допускается что то экспортировать
+*/
+
+import { initialCards } from "./cards.js";
+import { createCardElement, deleteCard, likeCard } from "./components/card.js";
+import { openModalWindow, closeModalWindow, setCloseModalWindowEventListeners } from "./components/modal.js";
+
+// DOM узлы
+const placesWrap = document.querySelector(".places__list");
+const profileFormModalWindow = document.querySelector(".popup_type_edit");
+const profileForm = profileFormModalWindow.querySelector(".popup__form");
+const profileTitleInput = profileForm.querySelector(".popup__input_type_name");
+const profileDescriptionInput = profileForm.querySelector(".popup__input_type_description");
+
+const cardFormModalWindow = document.querySelector(".popup_type_new-card");
+const cardForm = cardFormModalWindow.querySelector(".popup__form");
+const cardNameInput = cardForm.querySelector(".popup__input_type_card-name");
+const cardLinkInput = cardForm.querySelector(".popup__input_type_url");
+
+const imageModalWindow = document.querySelector(".popup_type_image");
+const imageElement = imageModalWindow.querySelector(".popup__image");
+const imageCaption = imageModalWindow.querySelector(".popup__caption");
+
+const openProfileFormButton = document.querySelector(".profile__edit-button");
+const openCardFormButton = document.querySelector(".profile__add-button");
+
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const profileAvatar = document.querySelector(".profile__image");
+
+const avatarFormModalWindow = document.querySelector(".popup_type_edit-avatar");
+const avatarForm = avatarFormModalWindow.querySelector(".popup__form");
+const avatarInput = avatarForm.querySelector(".popup__input");
+
+const handlePreviewPicture = ({ name, link }) => {
+  imageElement.src = link;
+  imageElement.alt = name;
+  imageCaption.textContent = name;
+  openModalWindow(imageModalWindow);
+};
+
+const handleProfileFormSubmit = (evt) => {
+  evt.preventDefault();
+  profileTitle.textContent = profileTitleInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  closeModalWindow(profileFormModalWindow);
+};
+
+const handleAvatarFromSubmit = (evt) => {
+  evt.preventDefault();
+  profileAvatar.style.backgroundImage = `url(${avatarInput.value})`;
+  closeModalWindow(avatarFormModalWindow);
+};
+
+const handleCardFormSubmit = (evt) => {
+  evt.preventDefault();
+  placesWrap.prepend(
+    createCardElement(
+      {
+        name: cardNameInput.value,
+        link: cardLinkInput.value,
+      },
+      {
+        onPreviewPicture: handlePreviewPicture,
+        onLikeIcon: likeCard,
+        onDeleteCard: deleteCard,
+      }
+    )
+  );
+
+  closeModalWindow(cardFormModalWindow);
+};
+
+// EventListeners
+profileForm.addEventListener("submit", handleProfileFormSubmit);
+cardForm.addEventListener("submit", handleCardFormSubmit);
+avatarForm.addEventListener("submit", handleAvatarFromSubmit);
+
+openProfileFormButton.addEventListener("click", () => {
+  profileTitleInput.value = profileTitle.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
+  openModalWindow(profileFormModalWindow);
+});
+
+profileAvatar.addEventListener("click", () => {
+  avatarForm.reset();
+  openModalWindow(avatarFormModalWindow);
+});
+
+openCardFormButton.addEventListener("click", () => {
+  cardForm.reset();
+  openModalWindow(cardFormModalWindow);
+});
+
+// отображение карточек
+initialCards.forEach((data) => {
+  placesWrap.append(
+    createCardElement(data, {
+      onPreviewPicture: handlePreviewPicture,
+      onLikeIcon: likeCard,
+      onDeleteCard: deleteCard,
+    })
+  );
+});
+
+//настраиваем обработчики закрытия попапов
+const allPopups = document.querySelectorAll(".popup");
+allPopups.forEach((popup) => {
+  setCloseModalWindowEventListeners(popup);
+});
